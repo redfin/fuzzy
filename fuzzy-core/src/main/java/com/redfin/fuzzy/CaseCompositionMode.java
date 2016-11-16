8e5cd4c54dc8a5882be6b8bbf032035c38483b70
Mark Biddlecom <markbiddlecom@gmail.com>
2016-11-15 21:05:37 -0800

    Adding case composition utilities.

---
+++ fuzzy-core/src/main/java/com/redfin/fuzzy/CaseCompositionMode.java
@@ -0,0 +1,151 @@
+package com.redfin.fuzzy;
+
+import com.redfin.fuzzy.pairwise.Pairwise;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.Stack;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * Describes the different algorithms the fuzzy library can use to permute subcases when using one of the
+ * {@linkplain Cases#compose Case composition} functions.
+ */
+public enum CaseCompositionMode {
+	/**
+	 * The composition will generate enough subcases so that each base case's suppliers are covered at least once, or
+	 * the a number equivalent to the maximum number of suppliers of any base case. Use this mode when you are concerned
+	 * about the number of combined cases growing too large.
+	 *
+	 * <p>
+	 * For example, suppose you are writing a composition of the following three base cases:
+	 * </p>
+	 * <ul>
+	 *     <li><strong>case {@code a}</strong>, with subcases {@code 1} and {@code 2};</li>
+	 *     <li><strong>case {@code b}</strong>, with subcases {@code 'X'}, {@code 'Y'}, and {@code 'Z'}; and</li>
+	 *     <li><strong>case {@code c}</strong>, with a single subcase {@code true}.</li>
+	 * </ul>
+	 * <p>
+	 * When these cases are composed with {@code EACH_SUBCASE_AT_LEAST_ONCE}, the final set of combined cases might be
+	 * the following:
+	 * </p>
+	 * <table>
+	 *     <thead>
+	 *         <tr><th></th><th>case {@code a}</th><th>case {@code b}</th><th>case {@code c}</th></tr>
+	 *     </thead>
+	 *     <tbody>
+	 *         <tr><th>1</th><td>{@code 1}</td><td>{@code 'X'}</td><td>{@code true}</td></tr>
+	 *         <tr><th>2</th><td>{@code 2}</td><td>{@code 'Y'}</td><td>{@code true}</td></tr>
+	 *         <tr><th>3</th><td>{@code 1}</td><td>{@code 'Z'}</td><td>{@code true}</td></tr>
+	 *     </tbody>
+	 * </table>
+	 * <p>
+	 * Note that the total number of composed subcases is 3, because case {@code b} had the most subcases (again, 3).
+	 * The subcases for {@code a} and {@code c} repeated as necessary so that each composed subcase had a valid value
+	 * for all three.
+	 * </p>
+	 * <p>
+	 * Note also that the way in which subcases are reused is not guaranteed by this algorithm and may change between
+	 * tests.
+	 * </p>
+	 */
+	EACH_SUBCASE_AT_LEAST_ONCE(baseCases -> {
+		List<Function[]> permutations = new ArrayList<>();
+		int[] indices = new int[baseCases.length];
+		int completed = 0;
+
+		Function[][] suppliers = new Function[baseCases.length][];
+		for(int i = 0; i < baseCases.length; i++) {
+			@SuppressWarnings("unchecked")
+			Set<Function> subcases = baseCases[i].getSuppliers();
+
+			Function[] subcasesArray = subcases.toArray(new Function[] {});
+			suppliers[i] = subcasesArray;
+		}
+
+		while(completed < baseCases.length) {
+			Function[] subcase = new Function[baseCases.length];
+			for(int i = 0; i < baseCases.length; i++) {
+				subcase[i] = suppliers[i][indices[i] % suppliers[i].length];
+				if(++indices[i] == suppliers[i].length) { completed++; }
+			}
+			permutations.add(subcase);
+		}
+
+		return permutations.toArray(new Function[][] {});
+	}),
+
+	/**
+	 * The composition will generate enough subcases so that each possible pairing of any two base cases will be
+	 * included at least once. Use this mode when you want to provide broader test coverage at the expense of a larger
+	 * number of permutations.
+	 *
+	 * <p>
+	 * This mode uses the pairwise algorithm defined by the {@link com.redfin.fuzzy.pairwise.Pairwise Pairwise} class;
+	 * refer to it for more details. As an example, consider the following three base cases:
+	 * </p>
+	 * <ul>
+	 *     <li><strong>case {@code a}</strong>, with subcases {@code 1} and {@code 2};</li>
+	 *     <li><strong>case {@code b}</strong>, with subcases {@code 'X'}, {@code 'Y'}, and {@code 'Z'}; and</li>
+	 *     <li><strong>case {@code c}</strong>, with a single subcase {@code true}.</li>
+	 * </ul>
+	 * <p>
+	 * Given this input, the pairwise algorithm might generate the following composed subcases:
+	 * </p>
+	 * <table>
+	 *     <thead>
+	 *         <tr><th></th><th>case {@code a}</th><th>case {@code b}</th><th>case {@code c}</th></tr>
+	 *     </thead>
+	 *     <tbody>
+	 *         <tr><th>1</th><td>{@code 1}</td><td>{@code 'X'}</td><td>{@code true}</td></tr>
+	 *         <tr><th>2</th><td>{@code 1}</td><td>{@code 'Y'}</td><td>{@code true}</td></tr>
+	 *         <tr><th>3</th><td>{@code 1}</td><td>{@code 'Z'}</td><td>{@code true}</td></tr>
+	 *         <tr><th>4</th><td>{@code 2}</td><td>{@code 'X'}</td><td>{@code true}</td></tr>
+	 *         <tr><th>5</th><td>{@code 2}</td><td>{@code 'Y'}</td><td>{@code true}</td></tr>
+	 *         <tr><th>6</th><td>{@code 2}</td><td>{@code 'Z'}</td><td>{@code true}</td></tr>
+	 *     </tbody>
+	 * </table>
+	 * <p>
+	 * Note that each possible pair between ({@code a}, {@code b}), ({@code a}, {@code c}), and ({@code b}, {@code c})
+	 * are covered at least once. The number of subcases necessary roughly depends on the number of subcases between the
+	 * two largest base cases (in this example, case {@code a} had two possible subcases, and case {@code b} had three,
+	 * for a total of {@code 2 * 3 = 6} cases).
+	 * </p>
+	 * <p>
+	 * Note also that the specific distribution of subcases is not guaranteed by this algorithm and may change between
+	 * tests.
+	 * </p>
+	 */
+	PAIRWISE_PERMUTATIONS_OF_SUBCASES(baseCases -> {
+		List<Set> parameters = Arrays
+			.stream(baseCases)
+			.map((Function<Case, Set>) Case::getSuppliers)
+			.collect(Collectors.toList());
+
+		Pairwise<Set> p = new Pairwise<>(parameters);
+		Stack<List<Object>> permutations = p.generate();
+
+		Function[][] subcases = new Function[permutations.size()][];
+		int i = 0;
+		for(List<Object> subcase : permutations) {
+			Function[] suppliers = new Function[baseCases.length];
+			for(int j = 0; j < baseCases.length; j++) {
+				suppliers[j] = (Function)subcase.get(j);
+			}
+			subcases[i++] = suppliers;
+		}
+
+		return subcases;
+	}),
+
+	;
+
+	/* package */ final Algorithm algorithm;
+
+	private CaseCompositionMode(Algorithm algorithm) { this.algorithm = algorithm; }
+
+	interface Algorithm { Function[][] apply(Case[] baseCases); }
+}
--- fuzzy-core/src/test/java/com/redfin/fuzzy/CasesTest.java
+++ fuzzy-core/src/test/java/com/redfin/fuzzy/CasesTest.java
@@ -9,8 +9,124 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class CasesTest {
+
+	@Test
+	public void testComposePairwise() {
+		Case<Integer> composedCase = Cases.compose(
+			CaseCompositionMode.PAIRWISE_PERMUTATIONS_OF_SUBCASES,
+			new Case[] { Any.of(2, 3), Any.of(10, 100, 1000) },
+			(random, values) -> (int)values[0] * (int)values[1]
+		);
+
+		Set<Integer> actuals = composedCase.generateAllOnce();
+
+		assertEquals(Util.setOf(20, 200, 2000, 30, 300, 3000), actuals);
+	}
+
+	@Test
+	public void testComposeAtLeastOnce() {
+		Case<Integer> composedCase = Cases.compose(
+			CaseCompositionMode.EACH_SUBCASE_AT_LEAST_ONCE,
+			new Case[] { Any.of(2, 3), Any.of(10, 100, 1000) },
+			(random, values) -> (int)values[0] * (int)values[1]
+		);
+
+		Set<Integer> actuals = composedCase.generateAllOnce();
+
+		assertEquals(3, actuals.size());
+
+		Set<Integer> expecteds = Util.setOf(20, 200, 2000, 30, 300, 3000);
+		assertFalse(expecteds.stream().anyMatch(i -> !expecteds.contains(i)));
+	}
+
+	@Test
+	public void testCompose1() {
+		Case<String> subject = Cases.compose(Literal.value(1), (random, a) -> "" + a);
+		assertEquals(Util.setOf("1"), subject.generateAllOnce());
+	}
+
+	@Test
+	public void testCompose2() {
+		Case<String> subject = Cases.compose(
+			CaseCompositionMode.EACH_SUBCASE_AT_LEAST_ONCE,
+			Literal.value(1), Literal.value(2),
+			(random, a, b) -> "" + a + b
+		);
+		assertEquals(Util.setOf("12"), subject.generateAllOnce());
+	}
+
+	@Test
+	public void testCompose3() {
+		Case<String> subject = Cases.compose(
+			CaseCompositionMode.EACH_SUBCASE_AT_LEAST_ONCE,
+			Literal.value(1), Literal.value(2), Literal.value(3),
+			(random, a, b, c) -> "" + a + b + c
+		);
+		assertEquals(Util.setOf("123"), subject.generateAllOnce());
+	}
+
+	@Test
+	public void testCompose4() {
+		Case<String> subject = Cases.compose(
+			CaseCompositionMode.EACH_SUBCASE_AT_LEAST_ONCE,
+			Literal.value(1), Literal.value(2), Literal.value(3), Literal.value(4),
+			(random, a, b, c, d) -> "" + a + b + c + d
+		);
+		assertEquals(Util.setOf("1234"), subject.generateAllOnce());
+	}
+
+	@Test
+	public void testCompose5() {
+		Case<String> subject = Cases.compose(
+			CaseCompositionMode.EACH_SUBCASE_AT_LEAST_ONCE,
+			Literal.value(1), Literal.value(2), Literal.value(3), Literal.value(4), Literal.value(5),
+			(random, a, b, c, d, e) -> "" + a + b + c + d + e
+		);
+		assertEquals(Util.setOf("12345"), subject.generateAllOnce());
+	}
+
+	@Test
+	public void testCompose6() {
+		Case<String> subject = Cases.compose(
+			CaseCompositionMode.EACH_SUBCASE_AT_LEAST_ONCE,
+			Literal.value(1), Literal.value(2), Literal.value(3), Literal.value(4), Literal.value(5), Literal.value(6),
+			(random, a, b, c, d, e, f) -> "" + a + b + c + d + e + f
+		);
+		assertEquals(Util.setOf("123456"), subject.generateAllOnce());
+	}
+
+	@Test
+	public void testCompose7() {
+		Case<String> subject = Cases.compose(
+			CaseCompositionMode.EACH_SUBCASE_AT_LEAST_ONCE,
+			Literal.value(1), Literal.value(2), Literal.value(3), Literal.value(4), Literal.value(5), Literal.value(6), Literal.value(7),
+			(random, a, b, c, d, e, f, g) -> "" + a + b + c + d + e + f + g
+		);
+		assertEquals(Util.setOf("1234567"), subject.generateAllOnce());
+	}
+
+	@Test
+	public void testCompose8() {
+		Case<String> subject = Cases.compose(
+			CaseCompositionMode.EACH_SUBCASE_AT_LEAST_ONCE,
+			Literal.value(1), Literal.value(2), Literal.value(3), Literal.value(4), Literal.value(5), Literal.value(6), Literal.value(7), Literal.value(8),
+			(random, a, b, c, d, e, f, g, i) -> "" + a + b + c + d + e + f + g + i
+		);
+		assertEquals(Util.setOf("12345678"), subject.generateAllOnce());
+	}
+
+	@Test
+	public void testCompose9() {
+		Case<String> subject = Cases.compose(
+			CaseCompositionMode.EACH_SUBCASE_AT_LEAST_ONCE,
+			Literal.value(1), Literal.value(2), Literal.value(3), Literal.value(4), Literal.value(5), Literal.value(6), Literal.value(7), Literal.value(8), Literal.value(9),
+			(random, a, b, c, d, e, f, g, i, j) -> "" + a + b + c + d + e + f + g + i + j
+		);
+		assertEquals(Util.setOf("123456789"), subject.generateAllOnce());
+	}
 
 	@Test
 	public void testMap() {
